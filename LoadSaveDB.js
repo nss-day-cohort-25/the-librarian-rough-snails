388bf52f80500a775dabec8518d5a427582f70c2
rafcevallos <rafael.cevallos81@gmail.com>
2018-04-20 14:49:11 -0500

    Added Save Database and 1 Function for Loading Databases

--- LoadSaveDB.js
+++ LoadSaveDB.js
@@ -1,26 +1,22 @@
-// SAVING AND LOADING CUSTOMER DATABASE TO/FROM LOCAL STORAGE
-// Function to load Customer Database saved in browser
-const loadDatabase = function(customerDB) {
+// Function to SAVE Customer Database 
+localStorage.setItem("Customer Database", JSON.stringify(customerDB))
 
-    // Get the string version of the Customer Database
-    const databaseString = localStorage.getItem("Customer Database")
+// Function to SAVE Library Database
+localStorage.setItem("Library Database", JSON.stringify(libraryDB))
+
+
+// Function to LOAD either database
+const loadDatabase = function(DB) {
+    
+    // Get the string version of either database
+    const databaseString = localStorage.getItem(DB)
         
     // Use JSON.parse() to convert the string back into an object
     return JSON.parse(databaseString)
 
-    }
+}
     
-    
-// SAVING AND LOADING LIBRARY DATABASE TO/FROM LOCAL STORAGE
-// Function to load Library Database saved in browser
-const loadDatabase = function(libraryDB) {
-    
-    // Get the string version of the Library Database
-    const databaseString = localStorage.getItem("Library Database")
-        
-    // Use JSON.parse() to convert the string back into an object
-    return JSON.parse(databaseString)
-
-    }
+loadDatabase("Library Database");
+loadDatabase("Customer Database");
     
         
